e03f97665a9b5c18c9173adfc0681145c1d3b363
Carrington <mcarringtone@gmail.com>
2022-05-20 21:57:43 -0400

    weekday array

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const btn = document.getElementById("button");
 
 let dateGrab = new Date();
+
+let weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
