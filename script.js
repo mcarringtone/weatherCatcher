91a6f8b570e89798f57ab069e72467f4232df019
Carrington <mcarringtone@gmail.com>
2022-05-20 22:03:39 -0400

    catch

--- script.js
+++ script.js
@@ -20,24 +20,31 @@ function weatherQuery() {
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cityRefresh.value +
       "&appid=e458f3719040b92db51cd836f40d7269"
-  )   
-   .then((res) => res.json())
+  )
+    .then((res) => res.json())
     .then((data) => {
       for (let i = 0; i < 5; i++) {
         document.getElementById("day" + (i + 1) + "Min").innerHTML =
           "Min: " +
           Number(data.list[i].main.temp_min - 273.15).toFixed(1) +
           "°";
-      } 
+      }
       for (i = 0; i < 5; i++) {
         document.getElementById("day" + (i + 1) + "Max").innerHTML =
           "Max: " +
           Number(data.list[i].main.temp_max - 273.15).toFixed(2) +
           "°";
       }
-       for (i = 0; i < 5; i++) {
+      for (i = 0; i < 5; i++) {
         document.getElementById("img" + (i + 1)).src =
           "http://openweathermap.org/img/wn/" +
           data.list[i].weather[0].icon +
           ".png";
       }
+      console.log(data);
+    })
+
+    .catch((err) =>
+      alert("Something Went Wrong: Try Checking Your Internet Coneciton")
+    );
+}
